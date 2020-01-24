b1978aa1e4758888776cb8876c7412f376cb04fe
David Quintanar <davidquintanar@hotmail.com>
2020-01-24 16:22:59 -0700

    add to JsonFileProductService class with Deserialize and PropertyNameCaseInsensitive

--- EclecticCrafts.WebSite/Services/JsonFileProductService.cs
+++ EclecticCrafts.WebSite/Services/JsonFileProductService.cs
@@ -15,13 +15,22 @@ namespace EclecticCrafts.WebSite.Services
         }
 
         public IWebHostEnvironment WebHostEnvironment { get; }
-        
-        
 
         private string JsonFileName
         {
             get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
         }
 
+        public IEnumerable<Product> GetProducts()
+        {
+            using(var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
     }
 }
