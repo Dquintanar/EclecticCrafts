e77152da34bde258c3e5a7dbe9ea0f271c9fbf00
David Quintanar <davidquintanar@hotmail.com>
2020-01-25 08:36:13 -0700

    add to Index.cshtml.cs the JsonFileProductService ProductService logger

--- EclecticCrafts.WebSite/Pages/Index.cshtml.cs
+++ EclecticCrafts.WebSite/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EclecticCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,10 +12,13 @@ namespace EclecticCrafts.WebSite.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, 
+            JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
         public void OnGet()
